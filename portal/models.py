ca4c4f7dfda31578a101d8e43681bb0c0782febe
sandeepsubramaniyan <sandeepkathiravan@gmail.com>
2022-09-19 23:42:08 +0530

    created file field and changed file_type

--- portal/models.py
+++ portal/models.py
@@ -5,7 +5,8 @@ class MyFileUpload(models.Model):
     username = models.CharField(max_length=100)
     uploaded_at = models.DateTimeField(blank=True, null=True)
     file_name = models.CharField(max_length=50)
-    file_type = models.FileField(upload_to='documents')
+    file_type = models.CharField(max_length=50)
+    file = models.FileField(upload_to='documents')
     
     def upload(self):
         self.uploaded_at = timezone.now()
