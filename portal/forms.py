79f344522a191f9424ba26e206085d27396c687d
sandeepsubramaniyan <sandeepkathiravan@gmail.com>
2022-09-20 20:10:31 +0530

    imported Usercreationform for signup

--- portal/forms.py
+++ portal/forms.py
@@ -1,8 +1,18 @@
 from django import forms
 from .models import MyFileUpload
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 class MyFileForm(forms.ModelForm):
     
     class Meta:
         model = MyFileUpload
         fields = '__all__'
+        
+
+class SignUpForm(UserCreationForm):
+    
+    class Meta:
+        model=User
+        fields =['username','password1','password2']        
