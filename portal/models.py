abc4f3cac1318f4fca8e6f4c9a9fb44bc5f24ca3
sandeepsubramaniyan <sandeepkathiravan@gmail.com>
2022-09-20 17:25:28 +0530

    changed upload field to auto

--- portal/models.py
+++ portal/models.py
@@ -1,16 +1,13 @@
+from pickle import FALSE
 from django.db import models
-from django.utils import timezone
+
 
 class MyFileUpload(models.Model):
     username = models.CharField(max_length=100)
-    uploaded_at = models.DateTimeField(blank=True, null=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
     file_name = models.CharField(max_length=50)
     file_type = models.CharField(max_length=50)
-    file = models.FileField(upload_to='documents')
-    
-    def upload(self):
-        self.uploaded_at = timezone.now()
-        self.save()
-        
+    file = models.FileField(upload_to='documents')    
+            
     def __str__(self):
-        self.username
+        return self.username
