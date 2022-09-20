783861b4d33440575b67e00ec05569ddf57f788d
sandeepsubramaniyan <sandeepkathiravan@gmail.com>
2022-09-20 17:30:05 +0530

    uploading a file and saves in database

--- portal/views.py
+++ portal/views.py
@@ -1,13 +1,37 @@
+from genericpath import exists
 from django.shortcuts import render
-
 from portal.forms import MyFileForm
-
+from portal.models import MyFileUpload
+from django.contrib import messages
+from django.shortcuts import redirect
+import datetime
+ 
 # Create your views here.
 
 def home(request):
-    myform =MyFileForm
-    context={'myform':myform}
+    myform = MyFileForm
+    context = {'myform':myform}
     return render(request,'upload.html',context)
 
-
+def upload(request):
+    
+    data = MyFileUpload.objects.all()
+    
+    if request.method =="POST":
+        myform = MyFileForm(request.POST,request.FILES)
+        if myform.is_valid():
+            
+            MyUserName = request.POST.get('username')
+            MyFileName = request.POST.get('file_name')
+            MyFileType = request.POST.get('file_type')
+            MyFile = request.FILES.get('file')
+            
+            exists = MyFileUpload.objects.filter(file=MyFile).exists()
+            
+            if exists:
+                messages.error(request,'The file %s is already exists...!!!'% MyFile)
+            else:
+                MyFileUpload.objects.create(username=MyUserName,file_name=MyFileName,file_type=MyFileType,file=MyFile).save()
+                messages.success(request,"file uploaded successfully")
+        return redirect('home')
     
