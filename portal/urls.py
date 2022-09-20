bcf9c1bb170a1480925d507be9cb2f077854b43b
sandeepsubramaniyan <sandeepkathiravan@gmail.com>
2022-09-21 01:46:08 +0530

    added login as default url

--- portal/urls.py
+++ portal/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('profile/',views.profile,name='profile'),
     path('upload/',views.upload,name='upload'),
     path('signup/',views.signup,name='signup'),
-    
+    path('',views.login, name='login'),
+    path('home/',views.home,name='home'),
     
 ]
