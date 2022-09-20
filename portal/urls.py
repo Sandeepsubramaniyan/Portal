aed69dee8415a1011a2bc87517626eec84358084
sandeepsubramaniyan <sandeepkathiravan@gmail.com>
2022-09-20 17:26:51 +0530

    uncommented lines

--- portal/urls.py
+++ portal/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('home/',views.home,name='home'),
-    # path('upload/',views.upload,name='upload'),
+    path('',views.home,name='home'),
+    path('upload/',views.upload,name='upload'),
     
     
 ]
