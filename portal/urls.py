95aef4ded3053d3d105295133f490af6ccea3bd3
sandeepsubramaniyan <sandeepkathiravan@gmail.com>
2022-09-23 14:30:06 +0530

    removed url for profile

--- portal/urls.py
+++ portal/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 
 urlpatterns = [
-    path('profile/',views.profile,name='profile'),
     path('upload/',views.upload,name='upload'),
     path('signup/',views.signup,name='signup'),
     path('',views.login, name='login'),
     path('home/',views.home,name='home'),
+    path('index/',views.index,name='index'),
     
 ]
